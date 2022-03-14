74f78095c5f8f943c07fc639149320a4138b614d
ascUin <leeuin2716@gmail.com>
2022-03-14 14:20:13 +0900

    매니두매니 원투원 필드

--- instagram/models.py
+++ instagram/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank =True, upload_to = uuid_name_upload_to)
+    tag_set = models.ManyToManyField('Tag', blank=True)
     is_public = models.BooleanField(default=False,verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -32,3 +33,12 @@ class Comment(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    # post_set = models.ManyToManyField(Post)
+    
+    def __str__(self):
+        return self.name
