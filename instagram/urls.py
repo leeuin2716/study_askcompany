b7fb0b3a8833e726e63fe60346db0e9b22d24d19
ascUin <leeuin2716@gmail.com>
2022-03-14 17:02:07 +0900

    음당의함수 기반뷰

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list),
+    path('<int:pk>', views.post_detail),
+    # re_path(r'(?P<pk>\d+)/$', views.post_detial),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,16 +1,28 @@
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Post
 
 
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q','')
-    if q:
-        qs = qs.filter(message__icontains=q)
+
+post_list = ListView.as_view(model=Post)
+
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q','')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
         
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html',{
-        'post_list':qs,
-        'q':q,
-    })        
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html',{
+#         'post_list':qs,
+#         'q':q,
+#     })        
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    response = HttpResponse()
+    response.write("Hello Word")
+    return response
