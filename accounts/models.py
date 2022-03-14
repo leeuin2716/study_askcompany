1fd29ea298d67e51dfa53e20e2187b9d15ab28c4
ascUin <leeuin2716@gmail.com>
2022-03-14 10:19:04 +0900

    onetoone 필드

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=6) # , validators=[])
