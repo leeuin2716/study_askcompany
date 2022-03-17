14235d71071f170f0d5798ca7fb5b9a8091a610a
ascUin <leeuin2716@gmail.com>
2022-03-17 16:31:54 +0900

    주피터 파일 위치변경 및 CBV

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,8 +23,8 @@ register_converter(YearConverter, 'year')
 app_name = 'instagram' # URL Reverse 에서 namespace 역할을 하게됨
 
 urlpatterns = [
-    path('',views.post_list),
-    path('<int:pk>', views.post_detail),
+    path('',views.post_list, name='post_list'),
+    path('<int:pk>', views.post_detail, name='post_detail'),
     # re_path(r'(?P<pk>\d+)/$', views.post_detial),
     # path('archives/<int:year>/', views.archives_year),
     # re_path(r'archives/(?P<year>\d+)/', views.archives_year),
--- instagram/views.py
+++ instagram/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpRequest, HttpResponse
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.http import Http404, HttpRequest, HttpResponse
+from django.views.generic import ListView,DetailView
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 
-
-
-
-post_list = ListView.as_view(model=Post)
 
 
 # def post_list(request):
@@ -22,11 +18,58 @@ post_list = ListView.as_view(model=Post)
 #     })        
 
 
-def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    response = HttpResponse()
-    response.write("Hello Word")
-    return response
 
+post_list = ListView.as_view(model=Post)
+# post_list = ListView.as_view(model=Post, paginate_by=10)
+
+# class PostListView(ListView):
+#     model = Post
+#     paginate_by = 10
+# post_list = PostListView.as_view()
+
+
+# class PostListView(ListView):
+#     model = Post
+#     paginate_by = 10
+
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         qs = qs.filter(...)
+#         return qs
+
+# post_list  =  PostListView.as_view()
+
+
+# def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     post = get_object_or_404(Post, pk=pk)
+#     # try:
+#     #     post = Post.objects.get(pk=pk)   #DoesNotExist 예외
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+    
+#     return render(request, 'instagram/post_detail.html',{
+#         'post':post,
+#     })
+    
+post_detail = DetailView.as_view(
+    model=Post,
+    queryset=Post.objects.filter(is_public=True))
+
+   
+    
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    # queryset = Post.objects.filter(is_public=True)
+    def get_queryset(self):
+        qs = super().get_queryset()
+        
+        if not self.request.user.is_authenticated:
+            qs = qs.filter(is_public=True)
+        return qs
+
+post_detail = PostDetailView.as_view()
 
 def archives_year(request, year):
     return HttpResponse(f"{year}년 archives_year")
