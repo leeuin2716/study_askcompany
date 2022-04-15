e9591b2bdd2b527844b7e36f42ebac4d19b63e3b
이유인 <leeuin2716>
2022-04-15 12:47:35 +0900

    createView 사용

--- instagram/views.py
+++ instagram/views.py
@@ -1,73 +1,75 @@
-from django.http import Http404, HttpRequest, HttpResponse
-from django.views.generic import ListView,DetailView,ArchiveIndexView,YearArchiveView,MonthArchiveView,WeekArchiveView,DayArchiveView,TodayArchiveView,DateDetailView
-from django.shortcuts import redirect, render,get_object_or_404
-from .models import Post
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
+    WeekArchiveView, DayArchiveView, TodayArchiveView, DateDetailView, CreateView
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render, get_object_or_404
 from django.utils.decorators import method_decorator
-from .forms import PostForm
-from django.db import models
-from django.contrib import messages
+from django.views.generic import ListView, DetailView, ArchiveIndexView, YearArchiveView, MonthArchiveView, \
+    WeekArchiveView, DayArchiveView, TodayArchiveView, DateDetailView
 
-@login_required
-def post_new(request):
-    # models.GenericIPAddressField   ip주소 저장가능
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user  # 현재 로그인 유저 Instance
-            post.save()
-            messages.success(request,'포스팅을 저장했습니다.')
-            return redirect(post)
-    else:
-        form = PostForm()
-    
-    
-    return render(request, 'instagram/post_form.html',{
-        'form':form,
-        'post':None,
-    })
+from .forms import PostForm
+from .models import Post
+
+
+# @login_required
+# def post_new(request):
+#     # models.GenericIPAddressField   ip주소 저장가능
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user  # 현재 로그인 유저 Instance
+#             post.save()
+#             messages.success(request, '포스팅을 저장했습니다.')
+#             return redirect(post)
+#     else:
+#         form = PostForm()
+#
+#     return render(request, 'instagram/post_form.html', {
+#         'form': form,
+#         'post': None,
+#     })
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+
+
 
 @login_required
 def post_edit(request, pk):
-    post = get_object_or_404(Post,pk=pk)
-    
-    #작성자 체크 tip 추후 장식자 로 만들어서 활용가능 
+    post = get_object_or_404(Post, pk=pk)
+
+    # 작성자 체크 tip 추후 장식자 로 만들어서 활용가능
     if post.author != request.user:
-        messages.error(request,'작성자만 수정 할 수 있습니다. ')
+        messages.error(request, '작성자만 수정 할 수 있습니다. ')
         return redirect(post)
-    
-      
+
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
-            messages.success(request,'포스팅을 수정했습니다.')
-            
+            messages.success(request, '포스팅을 수정했습니다.')
+
             return redirect(post)
     else:
         form = PostForm(instance=post)
-    
-    
-    return render(request, 'instagram/post_form.html',{
-        'form':form,
-        'post':post,
+
+    return render(request, 'instagram/post_form.html', {
+        'form': form,
+        'post': post,
     })
+
 
 @login_required
 def post_delete(request, pk):
-    post = get_object_or_404(Post,pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         post.delete()
-        messages.success(request,'포스팅을 삭제했습니다.')
+        messages.success(request, '포스팅을 삭제했습니다.')
         return redirect('instagram:post_list')
-    return render(request, 'instagram/post_confirm_delete.html',{
-        'post':post,
+    return render(request, 'instagram/post_confirm_delete.html', {
+        'post': post,
     })
-
-
-
-
 
 
 # @login_required
@@ -76,11 +78,11 @@ def post_delete(request, pk):
 #     q = request.GET.get('q','')
 #     if q:
 #         qs = qs.filter(message__icontains=q)
-    
-    
+
+
 #     messages.info(request,'messages 테스트')
 
-        
+
 #     # instagram/templates/instagram/post_list.html
 #     return render(request, 'instagram/post_list.html',{
 #         'post_list':qs,
@@ -88,9 +90,7 @@ def post_delete(request, pk):
 #     })        
 
 
-
 # post_list = ListView.as_view(model=Post)
-
 
 
 # post_list = login_required(ListView.as_view(model=Post, paginate_by=10))
@@ -100,8 +100,8 @@ class PostListView(ListView):
     model = Post
     paginate_by = 100
 
-post_list = PostListView.as_view()
 
+post_list = PostListView.as_view()
 
 
 # class PostListView(ListView):
@@ -122,64 +122,51 @@ post_list = PostListView.as_view()
 # post_list  =  PostListView.as_view()
 
 
-
-
-
-
-
-
 # def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
 #     post = get_object_or_404(Post, pk=pk)
 #     # try:
 #     #     post = Post.objects.get(pk=pk)   #DoesNotExist 예외
 #     # except Post.DoesNotExist:
 #     #     raise Http404
-    
+
 #     return render(request, 'instagram/post_detail.html',{
 #         'post':post,
 #     })
-    
+
 # post_detail = DetailView.as_view(
 #     model=Post,
 #     queryset=Post.objects.filter(is_public=True))
 
-   
-    
-    
 
 class PostDetailView(DetailView):
     model = Post
+
     # queryset = Post.objects.filter(is_public=True)
     def get_queryset(self):
         qs = super().get_queryset()
-        
+
         if not self.request.user.is_authenticated:
             qs = qs.filter(is_public=True)
         return qs
 
+
 post_detail = PostDetailView.as_view()
-
-
-
-
 
 # def archives_year(request, year):
 #     return HttpResponse(f"{year}년 archives_year")
 
 
-
 post_archive = ArchiveIndexView.as_view(model=Post, date_field='created_at', paginate_by=10)
 
+post_archive_year = YearArchiveView.as_view(model=Post, date_field='created_at', make_object_list=True)
 
+post_archive_month = MonthArchiveView.as_view(model=Post, date_field='created_at', month_format='%m')
 
-post_archive_year = YearArchiveView.as_view(model=Post, date_field='created_at' , make_object_list=True)
+post_archive_week = WeekArchiveView.as_view(model=Post, date_field='created_at',
+                                            week_format='%U')  # 한주시작 일요일 -> %U 월요일 -> %W
 
-post_archive_month = MonthArchiveView.as_view(model=Post, date_field='created_at',month_format='%m')
-
-post_archive_week = WeekArchiveView.as_view(model=Post, date_field='created_at', week_format = '%U')   #한주시작 일요일 -> %U 월요일 -> %W
-
-post_archive_day = DayArchiveView.as_view(model=Post, date_field='created_at',month_format='%m')
+post_archive_day = DayArchiveView.as_view(model=Post, date_field='created_at', month_format='%m')
 
 post_archive_today = TodayArchiveView.as_view(model=Post, date_field='created_at')
 
-post_archive_datedetial = DateDetailView.as_view(model=Post, date_field='created_at', month_format = '%m')
+post_archive_datedetial = DateDetailView.as_view(model=Post, date_field='created_at', month_format='%m')
