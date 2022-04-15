b8e21dea139f8640a83c8662799e66687f30b239
ascUin <leeuin2716@gmail.com>
2022-04-15 16:48:35 +0900

    4월 15일 로그인 처리 및 프로필 페이지 수정

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,36 +1,29 @@
-from django.urls import path, re_path , register_converter
+from django.urls import path, re_path, register_converter
 from . import views
 from .converters import DayConverter, MonthConverter, YearConverter
-
-
-
-
 
 register_converter(YearConverter, 'year')
 register_converter(MonthConverter, 'month')
 register_converter(DayConverter, 'day')
 
-
-
-app_name = 'instagram' # URL Reverse 에서 namespace 역할을 하게됨
+app_name = 'instagram'  # URL Reverse 에서 namespace 역할을 하게됨
 
 urlpatterns = [
-    path('new/',views.post_new, name='post_new'),
-    path('',views.post_list, name='post_list'),
+    path('new/', views.post_new, name='post_new'),
+    path('', views.post_list, name='post_list'),
     path('<int:pk>/', views.post_detail, name='post_detail'),
     path('<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('<int:pk>/delete/', views.post_delete, name='post_delete'),
-    
+
     # re_path(r'(?P<pk>\d+)/$', views.post_detial),
     # path('archives/<int:year>/', views.archives_year),
     # re_path(r'archives/(?P<year>\d+)/', views.archives_year),
     # re_path(r'archives/(?P<year>\d{4})/', views.archives_year),
     # re_path(r'archives/(?P<year>20\d{2})/', views.archives_year),
     # path('archives/<year:year>/', views.archives_year),
-    path('archive/',views.post_archive, name='post_archive'),
+    path('archive/', views.post_archive, name='post_archive'),
     path('archive/<year:year>/', views.post_archive_year, name='post_archive_year'),
     # path('archive/<year:year>/<month:month>/', views.post_archive_month, name='post_archive_month'),
     # path('archive/<year:year>/<month:month>/<day:day>/', views.post_archive_day, name='post_archive_day'),
-    
-    
+
 ]
